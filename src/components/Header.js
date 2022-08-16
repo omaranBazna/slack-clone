e62a75980bae1ad0191a0147ffc23a6b241f7c04
omaran bazna <omran4d@gmail.com>
2022-08-16 09:52:45 -0400

    add Avatar componenet

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "../styles/Header";
+import { Avatar } from "@material-ui/core";
 const Header = () => {
   return (
     <div className="header">
-      <div className="header-left"></div>
+      <div className="header-left">
+        <Avatar className="header-avatar" alt={"omaran bazna"} src={""} />
+      </div>
       <div className="header-search"></div>
       <div className="header-right"></div>
     </div>
