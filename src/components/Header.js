768ef915a6578a899bb74be06d45fdc7ad5b3d4f
omaran bazna <omran4d@gmail.com>
2022-08-16 00:15:24 -0400

    create the sections of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
 import React from "react";
 import "../styles/Header";
 const Header = () => {
-  return <div className="header"></div>;
+  return (
+    <div className="header">
+      <div className="header-left"></div>
+      <div className="header-search"></div>
+      <div className="header-right"></div>
+    </div>
+  );
 };
 
 export default Header;
