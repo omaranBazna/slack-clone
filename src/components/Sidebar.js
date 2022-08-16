f562ac8f33bfddb68b2a170c54e194d204a4157c
omaran bazna <omran4d@gmail.com>
2022-08-16 18:07:14 -0400

    add more styling to the side bar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,16 @@
 import React from "react";
 import "../styles/sidebar.css";
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <div className="sidebar-header"></div>
+      <div className="sidebar-header">
+        <h2>Omaran Server</h2>
+        <h3>
+          <FiberManualRecordIcon />
+          Omaran Bazna
+        </h3>
+      </div>
     </div>
   );
 };
