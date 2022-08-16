4b65770816bfa71571d161a7f57623a2d874e073
omaran bazna <omran4d@gmail.com>
2022-08-16 18:03:31 -0400

    add side bar header

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "../styles/sidebar.css";
 const Sidebar = () => {
-  return <div className="sidebar"></div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar-header"></div>
+    </div>
+  );
 };
 
 export default Sidebar;
