e694225cebaeac9f50f3102e2369abd3a9974e22
omaran bazna <omran4d@gmail.com>
2022-08-16 18:09:44 -0400

    add create icon

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "../styles/sidebar.css";
 import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
+import CreateIcon from "@mui/icons-material/Create";
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar-header">
-        <h2>Omaran Server</h2>
-        <h3>
-          <FiberManualRecordIcon />
-          Omaran Bazna
-        </h3>
+        <div className="sidebar-info">
+          <h2>Omaran Server</h2>
+          <h3>
+            <FiberManualRecordIcon />
+            Omaran Bazna
+          </h3>{" "}
+        </div>
+        <CreateIcon />
       </div>
     </div>
   );
