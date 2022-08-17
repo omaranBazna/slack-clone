34b015267463b94bb1b477e26220846837fffa69
omaran bazna <omran4d@gmail.com>
2022-08-17 14:22:04 -0400

    create chat input component

---
+++ src/components/ChatInput.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ChatInput = () => {
+  return (
+    <div className="chat-input">
+      <form>
+        <input></input>
+      </form>
+    </div>
+  );
+};
+
+export default ChatInput;
