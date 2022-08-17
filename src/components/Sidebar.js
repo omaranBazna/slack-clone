d465ddcdca667ae2a74583d012e8ffdd511d559b
omaran bazna <omran4d@gmail.com>
2022-08-16 23:03:05 -0400

    add sidebar options

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,6 +4,13 @@ import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 import CreateIcon from "@mui/icons-material/Create";
 import SidebarOption from "./SidebarOption";
 import InsertCommentIcon from "@mui/icons-material/InsertComment";
+import InboxIcon from "@mui/icons-material/Inbox";
+import DraftsIcon from "@mui/icons-material/Drafts";
+import FileCopyIcon from "@mui/icons-material/FileCopy";
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+import AppsIcon from "@mui/icons-material/Apps";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
 const Sidebar = () => {
   return (
     <div className="sidebar">
@@ -17,8 +24,14 @@ const Sidebar = () => {
         </div>
         <CreateIcon />
       </div>
-      <SidebarOption Icon={InsertCommentIcon} title="insert comment" />
-      <SidebarOption title="youtube" />
+      <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+      <SidebarOption Icon={InboxIcon} title="Mentions & reactions" />
+      <SidebarOption Icon={DraftsIcon} title="Saved items" />
+      <SidebarOption Icon={BookmarkIcon} title="Channel browser" />
+      <SidebarOption Icon={PeopleAltIcon} title="People & user groups" />
+      <SidebarOption Icon={AppsIcon} title="Apps" />
+      <SidebarOption Icon={FileCopyIcon} title="File browser" />
+      <SidebarOption Icon={ExpandLessIcon} title="Show less" />
     </div>
   );
 };
