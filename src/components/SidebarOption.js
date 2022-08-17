d6f8813f8f8fd0cc882c1ce2e42e41b4cbee20cd
omaran bazna <omran4d@gmail.com>
2022-08-16 22:36:48 -0400

    add sidebar option hover effect

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -2,9 +2,16 @@ import React from "react";
 import "../styles/sidebarOption.css";
 const SidebarOption = ({ Icon, title }) => {
   return (
-    <div>
+    <div className="sidebar-option">
       {Icon && <Icon className="sidebar-option-icon" />}
-      {Icon ? <h3>{title}</h3> : <h3>#{title}</h3>}
+      {Icon ? (
+        <h3>{title}</h3>
+      ) : (
+        <h3>
+          <span className="sidebar-option-hash">#</span>
+          {title}
+        </h3>
+      )}
     </div>
   );
 };
