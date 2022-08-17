df6063d030e9b3ee62570bd67d7e839f3ac7cc77
omaran bazna <omran4d@gmail.com>
2022-08-17 09:53:05 -0400

    routeing to the rooms

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,7 +2,13 @@ import React from "react";
 import "../styles/Chat.css";
 import { useParams } from "react-router-dom";
 const Chat = () => {
-  return <div className="chat"></div>;
+  const { roomId } = useParams();
+
+  return (
+    <div className="chat">
+      <h1>you are in room {roomId} </h1>
+    </div>
+  );
 };
 
 export default Chat;
