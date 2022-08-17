00bc959f6f1a766c626d3c6d48c256de3e421eaf
omaran bazna <omran4d@gmail.com>
2022-08-17 10:01:36 -0400

    add some content to the chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "../styles/Chat.css";
 import { useParams } from "react-router-dom";
+import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 const Chat = () => {
   const { roomId } = useParams();
 
   return (
     <div className="chat">
       <h1>you are in room {roomId} </h1>
+      <div className="chat-header">
+        <div className="chat-header-left">
+          <h4 className="chat-channel-name">
+            <strong>#{general}</strong>
+            <StarBorderOutlinedIcon />
+          </h4>
+        </div>
+        <div className="chat-header-right">
+          <p>
+            <InfoOutlinedIcon />
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
