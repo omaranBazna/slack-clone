a65e204278dfb04aae08bdd06754b6e28f3f149c
omaran bazna <omran4d@gmail.com>
2022-08-17 13:26:25 -0400

    create sign in button

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,12 +2,16 @@ import "../styles/Login.css";
 import React from "react";
 import { Button } from "@material-ui/core";
 const Login = () => {
+  const signIn = () => {};
   return (
     <div className="login">
       <div className="login-container">
         <img src="https://a.slack-edge.com/80588/marketing/img/icons/icon_slack_hash_colored.png" />
         <h1>Sign in to Slack CLone</h1>
-        <Button className="button"> Sign in with Google</Button>
+        <Button className="button" onClick={signIn}>
+          {" "}
+          Sign in with Google
+        </Button>
       </div>
     </div>
   );
