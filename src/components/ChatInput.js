8fd12e3d4387320a4928511ae959a49d782bb445
omaran bazna <omran4d@gmail.com>
2022-08-17 14:24:29 -0400

    handle send the chat message

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "../styles/ChatInput.css";
 const ChatInput = () => {
+  const sendMessage = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="chat-input">
       <form>
         <input></input>
+        <button type="submit" onClick={sendMessage}>
+          Send
+        </button>
       </form>
     </div>
   );
