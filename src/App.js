03514e1daae580b7dd00de648d5dff62bd8d0ee1
omaran bazna <omran4d@gmail.com>
2022-08-17 09:44:32 -0400

    add routes ,route to the main App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import {Routes,Route} from 'react-router-dom'
 function App() {
   return (
     <div lassName="app">
       <Header />
       <div className="app-body">
         <Sidebar />
+        <Routes>
+          <Route path="/" element={} />
+        </Routes>
       </div>
     </div>
   );
