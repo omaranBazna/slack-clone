7c0d1c6efd5eb7d3b62034f483cc58d3623b5b6f
omaran bazna <omran4d@gmail.com>
2022-08-17 12:58:15 -0400

    handel when there is no message

--- src/components/Message.js
+++ src/components/Message.js
@@ -3,11 +3,15 @@ import "../styles/Message.css";
 const Message = ({ message, timestamp, user, userPhoto }) => {
   return (
     <div className="message">
-      <img src={userPhoto} />
-      <div className="message-info">
-        <h4>{user}</h4> timestamp
-        <p>{message}</p>
-      </div>
+      {message && (
+        <>
+          <img src={userPhoto} />
+          <div className="message-info">
+            <h4>{user}</h4> timestamp
+            <p> message</p>
+          </div>
+        </>
+      )}
     </div>
   );
 };
