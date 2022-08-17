2bcacdfe4bce01ce02d8e70cb75cdd1c449f819b
omaran bazna <omran4d@gmail.com>
2022-08-17 12:46:26 -0400

    add content to message component

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,7 +1,15 @@
 import React from "react";
 import "../styles/Message.css";
-const Message = () => {
-  return <div></div>;
+const Message = ({ message, timestamp, user, userImg }) => {
+  return (
+    <div className="message">
+      <img src={userImg} />
+      <div className="message-info">
+        <h4>{user}</h4>
+        <p>{message}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Message;
