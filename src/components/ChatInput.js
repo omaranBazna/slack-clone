6d41754f44b73578c33f1a54b861d6e046309faa
omaran bazna <omran4d@gmail.com>
2022-08-17 14:28:48 -0400

    handle changeing the input in the chat send

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "../styles/ChatInput.css";
+import { useState } from "react";
 const ChatInput = ({ channelName, channelId }) => {
+  const [input, setInput] = useState("");
   const sendMessage = (e) => {
     e.preventDefault();
   };
   return (
     <div className="chat-input">
       <form>
-        <input placeholder={`Message #${channelName}`}></input>
+        <input
+          placeholder={`Message #${channelName}`}
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        ></input>
         <button type="submit" onClick={sendMessage}>
           Send
         </button>
