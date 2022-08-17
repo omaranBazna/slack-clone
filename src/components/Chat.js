08da17c5ebad0d0c6998d50f806441b438b72a05
omaran bazna <omran4d@gmail.com>
2022-08-17 09:51:56 -0400

    import useParams to the Chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,7 +1,8 @@
 import React from "react";
-
+import "../styles/Chat.css";
+import { useParams } from "react-router-dom";
 const Chat = () => {
-  return <div></div>;
+  return <div className="chat"></div>;
 };
 
 export default Chat;
