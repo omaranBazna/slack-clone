a9a47157c8e98a761927d7ea6f110af58e6573c1
omaran bazna <omran4d@gmail.com>
2022-08-17 14:59:42 -0400

    done with sending the message to the channels

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -1,10 +1,31 @@
 import React from "react";
 import "../styles/ChatInput.css";
 import { useState } from "react";
+import { useSelector } from "react-redux/es/exports";
+import { db } from "../firebase";
+import { doc, setDoc, collection, serverTimestamp } from "firebase/firestore";
+
 const ChatInput = ({ channelName, channelId }) => {
+  const userName = useSelector((state) => state.user.userName);
+  const userPhoto = useSelector((state) => state.user.userPhoto);
   const [input, setInput] = useState("");
-  const sendMessage = (e) => {
+
+  const sendMessage = async (e) => {
     e.preventDefault();
+    if (channelId) {
+      await setDoc(
+        doc(
+          collection(doc(collection(db, "rooms"), channelId), "messages"),
+          `${Math.floor(Math.random() * 10000000000)}`
+        ),
+        {
+          message: input,
+          user: userName,
+          userPhoto: userPhoto,
+          timestamp: serverTimestamp(new Date(Date.now())),
+        }
+      );
+    }
   };
   return (
     <div className="chat-input">
