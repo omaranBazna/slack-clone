f0fac89959394ad5b48518dae482a9721521f0f2
omaran bazna <omran4d@gmail.com>
2022-08-16 22:54:02 -0400

    styling the sidebar option

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -7,7 +7,7 @@ const SidebarOption = ({ Icon, title }) => {
       {Icon ? (
         <h3>{title}</h3>
       ) : (
-        <h3>
+        <h3 className="sidebar-option-channel">
           <span className="sidebar-option-hash">#</span>
           {title}
         </h3>
