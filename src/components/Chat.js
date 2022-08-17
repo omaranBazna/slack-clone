53115e5cc117606d0d2bc612e2a4cdb4b674ba38
omaran bazna <omran4d@gmail.com>
2022-08-17 10:39:12 -0400

    get channel name from data base

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -3,8 +3,29 @@ import "../styles/Chat.css";
 import { useParams } from "react-router-dom";
 import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import { db } from "../firebase";
+import { collection, onSnapshot, query, doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
 const Chat = () => {
   const { roomId } = useParams();
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    const docRef = doc(db, "rooms", roomId);
+    const getDocF = async () => {
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        setName(docSnap.data().name);
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    };
+
+    if (roomId) {
+      getDocF();
+    }
+  }, [roomId]);
 
   return (
     <div className="chat">
@@ -12,7 +33,7 @@ const Chat = () => {
       <div className="chat-header">
         <div className="chat-header-left">
           <h4 className="chat-channel-name">
-            <strong>#general</strong>
+            <strong>#{name}</strong>
             <StarBorderOutlinedIcon />
           </h4>
         </div>
